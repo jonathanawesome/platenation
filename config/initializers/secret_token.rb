742c1c8237467e3800840a711ceae36a852147fb
jonathanawesome <jon@safetyjon.com>
2013-03-04 13:46:10 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Platenation::Application.config.secret_token = '851a657e51a206ac8b46dc27bc47bec73c345308c33fe59cec22b52a9614013bd902894dbf9f5638255dc5f9617a5b55585fd1b222fc7ea146efdc9f8ae0f39a'
