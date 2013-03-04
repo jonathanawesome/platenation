a1f25e2ea45f00114f08a4baac90a30c3eaddb41
jonathanawesome <jon@safetyjon.com>
2013-03-04 14:01:08 -0600

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Platenation::Application.routes.draw do
+  root :to => 'pages#home'
+
+  match '/help',    to: 'pages#help'
+  match '/about',   to: 'pages#about'
+  match '/contact',   to: 'pages#contact'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ Platenation::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
